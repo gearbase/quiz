45c5bc7c27e690528ac69514c712e0b7ffbfaa74
gearbase <bsa@8731.com>
2020-03-27 15:32:18 +0300

    №2 Прикручена локализация

--- src/main/java/project/Main.java
+++ src/main/java/project/Main.java
@@ -3,19 +3,16 @@ package project;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import project.dao.InterviewDAO;
 import project.service.CheckInterview;
-
-import java.io.IOException;
 
 @ComponentScan
 @Configuration
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Main.class);
         context.getBean(CheckInterview.class).letsCheckInterview();
-        //InterviewDAO interviewDAO = context.getBean(InterviewDAO.class);
-        //System.out.println(interviewDAO.getInterviewList());
+        /*InterviewDAO interviewDAO = context.getBean(InterviewDAO.class);
+        System.out.println(interviewDAO.getInterviewList());*/
 
 
     }
