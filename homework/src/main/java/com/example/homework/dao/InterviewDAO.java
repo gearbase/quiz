cf5f76fb140a3f2ace38759efdf3da88a3f06441
gearbase <bsa8731@gmail.com>
2020-04-18 23:28:29 +0300

    Прикручена установка локали через Shell

--- homework/src/main/java/com/example/homework/dao/InterviewDAO.java
+++ homework/src/main/java/com/example/homework/dao/InterviewDAO.java
@@ -4,7 +4,9 @@ import com.example.homework.domain.Interview;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Locale;
 
 public interface InterviewDAO {
     List<Interview> getInterviewList() throws IOException;
+    void setLocale(Locale locale);
 }
--- homework/src/main/java/com/example/homework/shell/SpringShellConfig.java
+++ homework/src/main/java/com/example/homework/shell/SpringShellConfig.java
@@ -1,24 +1,37 @@
 package com.example.homework.shell;
 
+import com.example.homework.dao.InterviewDAO;
 import com.example.homework.service.CheckInterview;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellOption;
+
+import java.util.Locale;
 
 @ShellComponent
 public class SpringShellConfig {
     private final CheckInterview checkInterview;
+    private final InterviewDAO interviewDAO;
+
     //@Value("#{application['locale'].equals(\"ru_RU\")?\"старт\":\"start\"}")
     @Value("#{'${application.locale}'.equals(\"ru_RU\")?\"старт\":\"start\"}")
     public String value;
 
-    public SpringShellConfig(CheckInterview checkInterview) {
+    public SpringShellConfig(CheckInterview checkInterview, InterviewDAO interviewDAO) {
         this.checkInterview = checkInterview;
+        this.interviewDAO = interviewDAO;
     }
 
-    @ShellMethod(key = {"s","start"}, value = "Проведеине интервью")
-    public void checkInterview(){
+    @ShellMethod(key = {"s", "start"}, value = "Проведеине интервью")
+    public void checkInterview() {
         System.out.println(value);
         checkInterview.letsCheckInterview();
+    }
+
+    @ShellMethod(key = "l", value = "Установка локали")
+    public void setLocale(@ShellOption({"l, loc, locale"}) String locale) {
+        Locale loc = new Locale(locale);
+        interviewDAO.setLocale(loc);
     }
 }
